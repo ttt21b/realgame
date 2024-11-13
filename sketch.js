fbe863376c69d331dbda6fed13022a1f07a196c7
TIEN TRUONG <ttt21b@fsu.edu>
2024-11-13 04:00:53 -0500

    organized a lil

--- sketch.js
+++ sketch.js
@@ -56,24 +56,24 @@ function draw() {
 			let circleRadius = 300; // Circle radius (diameter was 600)
 			circle(mouseX, mouseY, circleRadius * 2); // Circle with a fixed diameter of 600
 
-			
-	
+
+
 
 			// Move the animal and check if it's inside the circle
 			animal.position.x += animal.vel.x;
 			animal.position.y += animal.vel.y;
 
-		
-				animal.visible = true;
-			
-			
+
+			animal.visible = true;
+
+
 			if (animal.position.x > windowWidth || animal.position.x < 0) {
 				animal.vel.x *= -1; // Reverse horizontal velocity
 			}
 			if (animal.position.y > windowHeight || animal.position.y < 0) {
 				animal.vel.y *= -1; // Reverse vertical velocity
 			}
-			
+
 			break;
 	}
 }
@@ -83,11 +83,11 @@ function mouseReleased() {
 	// Check if mouse clicked on the start button in stage 0
 	if (stage === 0 && mouseX >= 710 && mouseX <= 1110 && mouseY >= 700 && mouseY <= 900) {
 		stage = 1; // Transition to stage 1
-	} 
+	}
 	// Check if mouse clicked within the rectangle area in stage 1
 	else if (stage === 1 && mouseX >= 200 && mouseX <= 550 && mouseY >= 150 && mouseY <= 550) {
 		stage = 2; // Transition to stage 2
-	} 
+	}
 	// If in stage 2, reset to stage 0
 	else if (stage === 2) {
 		// Set a threshold distance (e.g., 20 pixels) to detect if the mouse is close to the animal
